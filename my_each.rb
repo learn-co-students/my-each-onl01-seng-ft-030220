a7dd701cf89a412c07977e240beae9641309a0ec
Amal Abdu <abduamal@ymail.com>
2020-03-08 06:29:51 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+buddies = ["joe", "flo", "moe"]
+
+def my_each(array)
+  i = 0 
+  
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
 end
+
+my_each(buddies) { |name| "Yo, " + name + "!"}
