0567f5a29a3135d7c1fa12b12237ac9ac64479e6
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-09 05:06:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+numbers=[1,2,3,4,5,6]
+
+def my_each (collection)
+  i=0
+  while i < collection.length
+    yield (collection[i])
+    i+=1
+  end
+  collection
 end
+
+my_each (numbers){|num| num.to_s}
